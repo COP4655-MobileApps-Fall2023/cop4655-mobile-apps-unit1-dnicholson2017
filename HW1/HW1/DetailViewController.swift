5a4707e1c8082878897495528b897b926417e396
D'Andre Nicholson <dnicholson2017@fau.edu>
2023-09-05 10:29:52 -0400

    feat: solution using button instead of tap

---
+++ HW1/HW1/DetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DetailViewController.swift
+//  HW1
+//
+//  Created by bianca nicholson on 9/5/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    var contextItem: ContextItem?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let contextItem = contextItem {
+            
+            titleLabel.text = contextItem.title
+            subtitleLabel.text = contextItem.subtitle
+            descriptionLabel.text = contextItem.description
+        }
+    }
+    
+    
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+
+
+    }
+    
+        
+}
--- HW1/HW1/ViewController.swift
+++ HW1/HW1/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var collegeLabel: UILabel!
+    
+    
+    @IBOutlet weak var roleLabel: UILabel!
+    
+    var contextItems = [
+        ContextItem(title: "Role", subtitle: "Software Engineer", description: "I develop mobile apps."), ContextItem(title: "Location", subtitle: "San Francisco, CA", description: "I live in the Bay Area."), ContextItem(title: "College", subtitle: "Stanford University", description: "I studied Computer Science.")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        photoImageView.image = UIImage(named: "my_photo")
+        roleLabel.text = contextItems[0].title + ": " + contextItems[0].subtitle
+        locationLabel.text = contextItems[1].title + ": " + contextItems[1].subtitle
+        collegeLabel.text = contextItems[2].title + ": " + contextItems[2].subtitle
     }
-
-
+    
+    @IBAction func roleButtonTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "showDetail", sender: contextItems[0])
+    }
+    
+    @IBAction func locationButtonTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "showDetail", sender: contextItems[1])
+    }
+    
+    @IBAction func collegeButtonTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "showDetail", sender: contextItems[2])
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let detailVC = segue.destination as? DetailViewController,
+                let contextItem = sender as? ContextItem {
+                detailVC.contextItem = contextItem
+            }
+        }
+    }
 }
-
